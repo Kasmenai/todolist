188bb67d986bee01a61ab1007617fe41ab1ce1d1
Kasmenai <kasmenai@gmail.com>
2019-04-12 01:50:24 +0300

    add material-ui components to form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react'
 import Store from '../context'
 import uniqid from 'uniqid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
 
 export default function TodoForm() {
   const {
     state: { currentTodo },
     dispatch,
   } = useContext(Store)
-  const { name, description, isEditing } = currentTodo
+  const { name, description, status, isEditing } = currentTodo
 
   const handleInputChange = e => {
     const target = e.target
@@ -29,39 +36,56 @@ export default function TodoForm() {
   }
 
   return (
-    <div className="row">
-      <div className="col-md-12">
-        <br />
-        <div className="input-group">
-          <form action="" onSubmit={handleTodoAdd}>
-            <input
-              className="form-control"
-              value={name}
-              onChange={handleInputChange}
-              name="name"
-              required
-            />
-            <input
-              className="form-control"
-              value={description}
-              onChange={handleInputChange}
-              name="description"
-            />
-            <div className="input-group-append">
-              <button className="btn btn-primary" type="submit">
-                Add
-              </button>
-              <button
-                className="btn btn-primary"
-                type="reset"
-                onClick={handleCancel}
-              >
-                cancel
-              </button>
-            </div>
-          </form>
+    <div className="sidebar">
+      <form onSubmit={handleTodoAdd} className="sidebar__form form">
+        <div className="form__header">
+          <Typography component="h1" variant="h2" gutterBottom>
+            Новая задача
+          </Typography>
         </div>
-      </div>
+        <div className="form__fields">
+          <TextField
+            label="Название задачи"
+            value={name}
+            onChange={handleInputChange}
+            name="name"
+            required
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            label="Описание задачи"
+            value={description}
+            onChange={handleInputChange}
+            name="description"
+            margin="normal"
+            variant="outlined"
+          />
+          <FormControl classes={{ root: 'form-control' }}>
+            <InputLabel htmlFor="status">Статус</InputLabel>
+            <Select
+              value={status}
+              onChange={handleInputChange}
+              inputProps={{
+                name: 'status',
+                id: 'status',
+              }}
+            >
+              <MenuItem value="Выполняется">Выполняется</MenuItem>
+              <MenuItem value="На потом">На потом</MenuItem>
+              <MenuItem value="Выполнена">Выполнена</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="form__footer">
+          <Button type="submit" color="primary" variant="contained">
+            Сохранить
+          </Button>
+          <Button color="secondary" variant="outlined" onClick={handleCancel}>
+            Отменить
+          </Button>
+        </div>
+      </form>
     </div>
   )
 }
