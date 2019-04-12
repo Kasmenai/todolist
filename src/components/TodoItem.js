02a27ad924682259bd9fa76cd58c60e20873b3a2
Kasmenai <kasmenai@gmail.com>
2019-04-12 22:46:10 +0300

    add LongMenu to table

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,12 +4,16 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import EditIcon from '@material-ui/icons/Edit'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
+import LongMenu from './LongMenu'
 
 const TodoItem = ({ todo, editTodo, deleteTodo, id }) => {
   return (
     <TableRow key={todo.id}>
       <TableCell>{id}</TableCell>
-      <TableCell>{todo.status}</TableCell>
+      <TableCell>
+        {todo.status}
+        <LongMenu id={todo.id} currentStatus={todo.status} />
+      </TableCell>
       <TableCell>{todo.name}</TableCell>
       <TableCell>{todo.description}</TableCell>
       <TableCell>
